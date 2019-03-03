0ca2d0f83ccc953ed5792832d8411c31a0919af1
ccs99301 <cs99301@gmail.com>
2019-03-03 20:49:56 +0800

    Update multiply.c

--- multiply.c
+++ multiply.c
@@ -2,21 +2,21 @@
 
 int main()
 {
-		int k;
-		while(1)
+	int k;
+	while(1)
+	{
+		printf("input:");
+		scanf("%d",&k);
+		if(k==0)
 		{
-				printf("input:");
-				scanf("%d",&k);
-				if(k==0)
-				{
-						printf("end!\n");
-						return 0;
-				}
-				else
-				{
-						float n=k*30.48;
-						printf("output:%f\n",n);
-				}
+			printf("end!\n");
+			return 0;
 		}
-		return 0;
+		else
+		{
+			float n=k*30.48;
+			printf("output:%f\n",n);
+		}
+	}
+	return 0;
 }
