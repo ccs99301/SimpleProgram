9f29170b7c2404cc3e8c2c97f342fc04c188806e
ccs99301 <cs99301@gmail.com>
2019-03-03 20:06:01 +0800

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+		int k;
+		while(1)
+		{
+				printf("input:");
+				scanf("%d",&k);
+				if(k==0)
+				{
+						printf("end!\n");
+						return 0;
+				}
+				else
+				{
+						float n=k*30.48;
+						printf("output:%f\n",n);
+				}
+		}
+		return 0;
+}
