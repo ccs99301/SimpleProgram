7438179215c56c099f2471c5f77db739d9860861
ccs99301 <cs99301@gmail.com>
2019-03-04 21:51:40 +0800

    Update and rename hw2.c to vowel.c

---
+++ vowel.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+	int total,c,vowel,num,n;
+	while ((c=getchar()) != EOF)
+	{
+		total++;
+		if(c=='a' || c=='A')
+			vowel++;
+		if(c=='e' || c=='E')
+			vowel++;
+		if(c=='i' || c=='I')
+			vowel++;
+		if(c=='o' || c=='O')
+			vowel++;
+		if(c=='u' || c=='U')
+			vowel++;
+		if(c>='0' && c<='9')		
+			num++;
+		if(c=='\n')
+			n++;
+	}
+	printf("Total characters:%d\n",total);
+	printf("Total rows:%d\n",n);
+	printf("Number:%d\n",num);
+	printf("Vowel:%d\n",vowel);
+	return 0;
+}
